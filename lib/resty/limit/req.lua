64998751cae0cab459b79d9f09ba01e20025299b
timebug <timebug.info@gmail.com>
2014-09-24 15:59:56 +0800

    set redis_limit_req_script_sha nil when eval failed

--- lib/resty/limit/req.lua
+++ lib/resty/limit/req.lua
@@ -84,6 +84,7 @@ local function redis_lookup(conn, zone, key, rate, duration)
     local res, err = red:evalsha(redis_limit_req_script_sha, 4,
                                  zone .. ":" .. key, rate, now, duration)
     if not res then
+        redis_limit_req_script_sha = nil
         return nil, err
     end
 
